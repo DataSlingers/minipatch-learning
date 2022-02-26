b977e95f1998d0c47991f55081b45d866716619c
T. Yao <ty13@rice.edu>
2022-02-25 19:04:30 -0600

    update configuration for sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -10,19 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+import sphinx_gallery
+import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath('..'))
 
 
-# -- Project information -----------------------------------------------------
 
-project = 'minipatch-learning'
-copyright = '2022, Tianyi Yao'
-author = 'Tianyi Yao'
-
-# The full version, including alpha/beta/rc tags
-release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,25 +28,103 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+    'numpydoc',
+    'sphinx_gallery.gen_gallery',
 ]
 
+# this is needed for some reason...
+# see https://github.com/numpy/numpydoc/issues/69
+numpydoc_show_class_members = False
+
+# pngmath / imgmath compatibility layer for different sphinx versions
+import sphinx
+from distutils.version import LooseVersion
+if LooseVersion(sphinx.__version__) < LooseVersion('1.4'):
+    extensions.append('sphinx.ext.pngmath')
+else:
+    extensions.append('sphinx.ext.imgmath')
+
+autodoc_default_flags = ['members', 'inherited-members']
+
+#autodoc_default_options = {"members": True, "inherited-members": True}
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
+
+# generate autosummary even if no references
+autosummary_generate = True
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# Generate the plots for the gallery
+plot_gallery = True
+
+# The master toctree document.
+master_doc = 'index'
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'minipatch-learning'
+copyright = '2022, Tianyi Yao'
+author = 'Tianyi Yao'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+from mplearn import __version__
+version = __version__
+# The full version, including alpha/beta/rc tags.
+release = __version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_templates', 'templates']
+
+
+# The reST default role (used for this markup: `text`) to use for all
+# documents.
+default_role = "literal"
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = False
 
 
 # -- Options for HTML output -------------------------------------------------
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+htmlhelp_basename = 'minipatch-learningdoc'
+
+
+trim_doctests_flags = True
+
+# sphinx-gallery configuration
+sphinx_gallery_conf = {
+    'doc_module': 'mplearn',
+    'backreferences_dir': os.path.join('generated'),
+    'reference_url': {
+        'mplearn': None}
+}
